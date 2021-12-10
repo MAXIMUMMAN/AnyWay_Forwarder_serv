f5ee3fbf43013afc40d0d6796762f6e3e5162014
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2021-12-10 21:23:54 +0300

    filters v 1.0

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get(
-    '/',
-)
+@app.get('/')
 async def get_cities():
     with open('data.json') as cities:
         response = json.load(cities)
         return response[0:len(response)]
+
+
+@app.get('/{num}')
+async def get_city(num: int):
+    with open('data.json') as cities:
+        response = json.load(cities)
+        return response[num]
+
+
+@app.get('/{filt}/{f}')
+async def get_city_filtered(sea_filt : int, moun_filt : int):
+    with open('data.json') as cities:
+        res = json.load(cities)
+        res2 = list(filter(lambda x: (x["sea"] >= sea_filt) and (x["mountains"] >= moun_filt), res))
+        return res2[0:len(res2)]
